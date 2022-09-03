3786ec1d14c72dc2ea4b86c232b446ebc6dcf0ba
willyanfx <willyan.fx@gmail.com>
2022-09-03 15:54:03 -0700

    create and add items

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,3 +1,53 @@
+use std::{collections::HashMap, io::Read, str::FromStr};
+struct Todo {
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        // insert a new item into our map.
+        // we pass true as value
+        self.map.insert(key, true);
+    }
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut file = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+    println!("{:?} {:?}", action, item);
+
+    let mut todo = Todo::new().expect("Initialisation of db failed");
+
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("TODO saved"),
+            Err(why) => println!("An error occured: {}", why),
+        }
+    }
 }
