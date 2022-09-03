8b4b69c25629b09b879a515fe7d8dfd8035f33d0
willyanfx <willyan.fx@gmail.com>
2022-09-03 16:02:57 -0700

    make it complete

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -4,11 +4,19 @@ struct Todo {
 }
 
 impl Todo {
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = false),
+            None => None,
+        }
+    }
+
     fn insert(&mut self, key: String) {
         // insert a new item into our map.
         // we pass true as value
         self.map.insert(key, true);
     }
+
     fn new() -> Result<Todo, std::io::Error> {
         let mut file = std::fs::OpenOptions::new()
             .write(true)
@@ -39,8 +47,6 @@ fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    println!("{:?} {:?}", action, item);
-
     let mut todo = Todo::new().expect("Initialisation of db failed");
 
     if action == "add" {
@@ -48,6 +54,14 @@ fn main() {
         match todo.save() {
             Ok(_) => println!("TODO saved"),
             Err(why) => println!("An error occured: {}", why),
+        }
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("'{}' is not present in the list", item),
+            Some(_) => match todo.save() {
+                Ok(_) => println!("todo saved"),
+                Err(why) => println!("An error occurred: {}", why),
+            },
         }
     }
 }
