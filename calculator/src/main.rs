f288ad7f895cdc2ba031dc1fe2047c14f0722ce1
willyanfx <willyan.fx@gmail.com>
2022-09-12 18:22:38 -0700

    basic functions for the calculator

---
+++ calculator/src/main.rs
@@ -0,0 +1,41 @@
+#[warn(dead_code)]
+use std::env::{args, Args};
+use std::os::macos::raw::stat;
+
+#[derive(Debug)]
+enum Operators {
+    Add,
+    Subtract,
+    Divide,
+    Multiply,
+}
+
+struct Calc {
+    first_number: i32,
+    operator: Operators,
+    second_number: i32,
+    result: i32,
+}
+
+fn main() {
+    let mut state = Calc {
+        first_number: 5,
+        operator: Operators::Add,
+        second_number: 2,
+        result: 0,
+    };
+
+    state.result = operate(state.operator, state.first_number, state.second_number);
+
+    println!("Result, {:?}", state.result);
+}
+
+fn operate(operator: Operators, first: i32, second: i32) -> i32 {
+    match operator {
+        Operators::Add => first + second,
+        Operators::Subtract => first - second,
+        Operators::Multiply => first * second,
+        Operators::Divide => first / second,
+        _ => 0,
+    }
+}
