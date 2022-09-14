dabf0585587cd2b40ec148e8560e070ea23870cc
willyanfx <willyan.fx@gmail.com>
2022-09-13 19:11:08 -0700

    key generater basic functions

---
+++ key_generator/src/main.rs
@@ -0,0 +1,66 @@
+fn main() {
+    results(26.0, 13.0, 1, Unit::Hour);
+}
+
+#[derive(Debug)]
+enum Unit {
+    Hour,
+    Second,
+}
+
+fn results(string_len: f32, length: f32, speed: i32, unit: Unit) {
+    let probability = 0.01; // 1/100
+    let speed_per_second = if let Unit::Hour = unit {
+        speed as f32 / 3600.0
+    } else {
+        speed as f32
+    };
+    println!("SPEED:::: {:?}", speed_per_second);
+
+    let rand: f32 = 61.105713;
+    println!(
+        "2481147136228978700:::: {:?}",
+        2_f32 * f32::powf(2_f32, rand)
+    );
+
+    let random_bits = get_random_bits(string_len, length);
+    let generate_for_collision = get_generate_for_collision(random_bits, probability);
+    let time_to_collision = get_time_to_collision(generate_for_collision, speed_per_second);
+}
+
+fn get_random_bits(string_length: f32, size: f32) -> f32 {
+    //  natural logarithm of 2, approximately 0.693
+    size * (string_length.ln() / 0.6931471805599453)
+}
+
+fn get_generate_for_collision(random_bits: f32, probability: f32) -> f32 {
+    // 0.010050335853501506
+    f32::sqrt((2_f32 * f32::powf(2_f32, random_bits)) * (1_f32 / (1_f32 - probability)).ln())
+}
+
+fn get_time_to_collision(generate_for_collision: f32, speed_per_second: f32) -> f32 {
+    (generate_for_collision / speed_per_second).floor()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_random_bits() {
+        assert_eq!(get_random_bits(26.0, 13.0), 61.105713);
+        assert_eq!(get_random_bits(46.0, 16.0), 88.37699);
+    }
+    #[test]
+    fn test_generate_for_collision() {
+        let probability = 0.01;
+        assert_eq!(
+            get_generate_for_collision(61.105713, probability),
+            223321470.0
+        );
+        assert_eq!(
+            get_generate_for_collision(88.37699, probability),
+            2842280000000.0
+        );
+    }
+}
