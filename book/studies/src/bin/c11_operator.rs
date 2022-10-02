ff8aab2f37e440205db35e9120cbe89007da3922
willyanfx <willyan.fx@gmail.com>
2022-10-02 11:19:38 -0700

    error handling

---
+++ book/studies/src/bin/c11_operator.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
+#[derive(Debug)]
+struct Info {
+    name: &'static str,
+    age: i32,
+    rating: i32,
+}
+
+fn write_info(info: &Info) -> io::Result<()> {
+    let mut file = File::create("best_friends.txt")?;
+    // ealy return on error
+    file.write_all(format!("name: {}\n", info.name).as_bytes())?;
+    file.write_all(format!("idade: {}\n", info.age).as_bytes())?; 
+    file.write_all(format!("avaliação: {}\n", info.rating).as_bytes())?;
+
+    Ok(())
+
+  }
+
+fn main() {
+    let friend01 = Info {
+        name: "Will G",
+        age: 35,
+        rating: 10
+    };
+
+    match write_info(&friend01) {
+        Err(e) => println!("Something went wrong -> {}", e),
+        Ok(()) => println!("All good"),
+    };
+}
---
+++ book/studies/src/bin/c11_test.rs
@@ -0,0 +1,40 @@
+#[derive(Debug)]
+struct Fibonacci {
+    curr: u32,
+    next: u32
+}
+
+impl Iterator for Fibonacci {
+    type Item = u32;
+
+
+    fn next(&mut self) -> Option<u32> {
+        let new_next = self.curr + self.next;
+
+        self.curr = self.next;
+        self.next = new_next;
+
+        // Fibonacci is infinite, however we will condition to max 21
+        
+        if self.curr > 21 {
+            None
+        } else {
+            Some(self.curr)
+        }
+
+    }
+
+}
+
+fn main() {
+    let fib1 = Fibonacci {curr: 1, next: 1};
+    println!("the first 4 number sequence is: ");
+    for i in fib1.take(4) {
+        println!("> {}", i);
+    }
+}
+
+#[test]
+fn name() {
+    assert_eq!(false, 'a' == 'a');
+}
