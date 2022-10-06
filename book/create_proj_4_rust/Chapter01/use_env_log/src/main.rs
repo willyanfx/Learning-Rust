d8c4d12ad704476096acc76d7f55f2c8c8513fc2
willyanfx <willyan.fx@gmail.com>
2022-10-05 22:49:22 -0700

    notes

--- book/create_proj_4_rust/Chapter01/use_env_log/src/main.rs
+++ book/create_proj_4_rust/Chapter01/use_env_log/src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate log;
+// use cmd  RUST_LOG=debug ./target/debug/use_env_log
+// define temp env variable RUST_LOG, debug is the value
 
 fn main() {
     env_logger::init();
