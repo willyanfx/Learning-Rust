454669109a8bc7bd6a8eb4a745b2a6145baff367
willyanfx <willyan.fx@gmail.com>
2022-10-06 09:27:18 -0700

    lazy initialize variable

---
+++ book/create_proj_4_rust/Chapter01/use_lazy_static/src/main.rs
@@ -0,0 +1,17 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref DICTIONARY: HashMap<u32, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(11, "foo");
+        m.insert(12, "bar");
+        println!("Initialized");
+        m
+    };
+}
+fn main() {
+    println!("Started");
+    println!("DICT contains {:?}", *DICTIONARY);
+    println!("DICT contains {:?}", *DICTIONARY);
+}
