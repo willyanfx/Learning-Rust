8ebb074028006d90ef49601226b1fa4ca7cff7d6
willyanfx <willyan.fx@gmail.com>
2022-10-05 22:09:15 -0700

    simple cmd line

---
+++ book/cmdline_args/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::str::FromStr;
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing arguments"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = num::integer::gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
