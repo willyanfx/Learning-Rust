76aee2cd11c164b5db6eea3d1c35b12bae455a08
willyanfx <willyan.fx@gmail.com>
2022-10-06 19:40:22 -0700

    dyn toml reader

---
+++ book/create_proj_4_rust/Chapter02/toml_dynamic/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // 1. Define the config structure.
+    let config_const_values = {
+        // 2. Get the path of the config file from the command line.
+        let config_path = std::env::args().nth(1).unwrap();
+        // 3. Load the whole file contents into a string.
+        let config_text = std::fs::read_to_string(&config_path).unwrap();
+        // 4. Load an unmutable config structure from the string.
+        config_text.parse::<toml::Value>().unwrap()
+    };
+
+    println!("Original: {:?}", config_const_values);
+
+    println!(
+        "[Postgresql].database: {}",
+        config_const_values
+            .get("postgresql")
+            .unwrap()
+            .get("database")
+            .unwrap()
+            .as_str()
+            .unwrap()
+    );
+}
