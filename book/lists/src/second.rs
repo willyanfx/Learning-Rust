0024eea4335a018e00ea8572cb9aff37158c33d2
willyanfx <willyan.fx@gmail.com>
2022-10-11 21:11:19 -0700

    iter

--- book/lists/src/second.rs
+++ book/lists/src/second.rs
@@ -1,10 +1,14 @@
-use std::{array::IntoIter, mem};
+use std::mem;
 
 pub struct List<T> {
     head: Link<T>,
 }
 
 pub struct IntoIter<T>(List<T>);
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
 
 type Link<T> = Option<Box<Node<T>>>;
 
@@ -20,6 +24,12 @@ impl<T> List<T> {
 
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
+    }
+
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
     }
 
     pub fn push(&mut self, elem: T) {
@@ -52,6 +62,18 @@ impl<T> Iterator for IntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    // Need it here too, this is a type declaration
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
     }
 }
 
@@ -126,5 +148,18 @@ mod tests {
             assert_eq!(iter.next(), Some(1));
             assert_eq!(iter.next(), None);
         }
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
