ef70c57cc55c751bea489a1bfbbf7a3996b5aa8e
willyanfx <willyan.fx@gmail.com>
2022-10-11 20:43:38 -0700

    Into Iter

--- book/lists/src/second.rs
+++ book/lists/src/second.rs
@@ -1,8 +1,10 @@
-use std::mem;
+use std::{array::IntoIter, mem};
 
 pub struct List<T> {
     head: Link<T>,
 }
+
+pub struct IntoIter<T>(List<T>);
 
 type Link<T> = Option<Box<Node<T>>>;
 
@@ -14,6 +16,10 @@ struct Node<T> {
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
     }
 
     pub fn push(&mut self, elem: T) {
@@ -39,6 +45,13 @@ impl<T> List<T> {
 
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.elem)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -99,5 +112,19 @@ mod tests {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+
+        #[test]
+        fn into_iter() {
+            let mut list = List::new();
+            list.push(1);
+            list.push(2);
+            list.push(3);
+
+            let mut iter = list.into_iter();
+            assert_eq!(iter.next(), Some(3));
+            assert_eq!(iter.next(), Some(2));
+            assert_eq!(iter.next(), Some(1));
+            assert_eq!(iter.next(), None);
+        }
     }
 }
